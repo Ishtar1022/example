46288b2aaaab94c4c3f152866226c50f6a93caac
ishtar <ishtar.f1022@gmail.com>
2023-03-10 11:16:29 +0800

    add some files

---
+++ strings.py
@@ -0,0 +1,71 @@
+# %%
+b = "banana"
+# %%
+c = b + " is a kind of fruit."
+# %%
+b * 3
+# %%
+len(b)
+# %%
+a_1 = "a b"
+# %%
+len(a_1)
+# %%
+print(a_1[1])
+# %%
+print(c)
+# %%
+dir(c)
+# %%
+c.title?
+# %%
+c.title()
+# %%
+c.capitalize()
+# %%
+c.upper()
+# %%
+c[0:3]
+# %%
+c[0:7]
+# %%
+c[0:6]
+# %%
+c[:6]
+# %%
+c[-6:len(c)]
+# %%
+c[-6:len(c)-1]
+# %%
+c.replace?
+# %%
+c.replace("a", "c")
+# %%
+c.replace("a", "c", 1)
+# %%
+dir?
+# %%
+c.split?
+# %%
+c.split()
+# %%
+c.split("kind")
+# %%
+c.split(" kind")
+# %%
+c.split(" kind ")
+# %%
+c.endwith?
+# %%
+c.endswith?
+# %%
+c.endswith("a")
+# %%
+c.endswith("fruit.")
+# %%
+c.index?
+# %%
+c.index("ana")
+# %%
+c[::2]
+# %%
